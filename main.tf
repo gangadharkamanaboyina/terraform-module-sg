8450abc6df9bf8edd281c6244e334482eb434406
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-11-11 07:05:30 +0530

    SG module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "main" {
   vpc_id = var.vpc_id
-  name = "${var.project}-${var.env}-${var.name}"
+  name = var.name
   dynamic "ingress" {
     for_each = toset(var.ingress_ports)
     content {
